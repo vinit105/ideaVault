48e3d4518dc37571f042575b6dcf7a1625d63d80
rishika638 <rishika82871@gmail.com>
2025-08-07 12:51:29 +0530

    final commit frontend functionality for submit-idea button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 import Home from './pages/Home';
 import Ideas from './pages/Ideas';
 import SubmitIdea from './pages/SubmitIdea';
